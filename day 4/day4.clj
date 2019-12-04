99053a75dcbd9a82c40520a0347bc70451df2758
Sam Gates <Sam.Gates@heliosinteractive.com>
2019-12-04 14:04:14 -0500

    remove unneeded calls

--- day 4/day4.clj
+++ day 4/day4.clj
@@ -3,7 +3,7 @@
 (def re ['#"(\d)\1{1,}" '#"^(?=\d{6}$)0*?1*?2*?3*?4*?5*?6*?7*?8*?9*?$"])
 
 (defn op [i]
-  (if (= i (first re)) (fn [x] (not (nil? (some #(= 2 (count (str (first %)))) x)))) #(pos? (count %))))
+  (if (= i (first re)) (fn [x] (some #(= 2 (count (str (first %)))) x)) #(pos? (count %))))
 
 (defn fits-all? [i n]
   (every? (partial (partial #((op %) (re-seq % (str n))))) i))
