857855777c1fa2c8b61fe39bce300c01844a721a
Sam Gates <sam.gates@heliosinteractive.com>
2019-12-03 22:30:52 -0500

    new approach

--- day 3/day3.clj
+++ day 3/day3.clj
@@ -3,66 +3,32 @@
 
 (defn abs [n] (max n (- n)))
 
-(defn read-and-split [path]
-  (map #(str/split % #",") (str/split (slurp path) #"\n")))
+(def input
+  (map #(str/split % #",") (str/split (slurp "day 3/input.txt") #"\n")))
+
+(defn movement [d]
+  (case d
+    "U" [0 1]
+    "D" [0 -1]
+    "R" [1 0]
+    "L" [-1 0]))
 
 (defn between [i s f]
   (and (<= s i) (>= f i)))
 
-(defn create-segment [s i]
-  (let [d (Integer. (re-find #"\d+" i))]
-    (case (str (first i))
-      "U" [(first s) (+ (second s) d)]
-      "D" [(first s) (- (second s) d)]
-      "L" [(- (first s) d) (second s)]
-      "R" [(+ (first s) d) (second s)]
-      ))
-  )
-
 (defn create-line [ins]
-  (loop [s [[[0 0] [0 0]]]
+  (loop [s [0 0]
          i 0]
     (if (<= (count ins) i)
       s
-      (recur (concat s [[(last (last s))(create-segment (last (last s)) (nth ins i))]]) (inc i))
-      )))
-
-(defn intersects [l1 l2]
-  (let [p11 (first l1) p12 (second l1)
-        p21 (first l2) p22 (second l2)
-        x11 (first p11) x12 (first p12)
-        y11 (second p11) y12 (second p12)
-        x21 (first p21) x22 (first p22)
-        y21 (second p21) y22 (second p22)
-        ta1 (+ (* (- y21 y22) (- x11 x21)) (* (- x22 x21) (- y11 y21)))
-        ta2 (- (* (- x22 x21) (- y11 y12)) (* (- x11 x12) (- y22 y21)))
-        tb1 (+ (* (- y11 y12) (- x11 x21)) (* (- x12 x11) (- y11 y21)))
-        tb2  (- (* (- x22 x21) (- y11 y12)) (* (- x11 x12) (- y22 y21)))]
-    (println (str p11 p12 p21 p22 ta1 " " ta2 " " tb1 " " tb2))
-    (if (or (= ta2 0) (= tb2 0))
-      1000000
-      (let [ta (/ ta1 ta2)
-            tb (/ tb1 tb2)]
-        (if (and (between ta 0 1) (between tb 0 1))
-          (do
-            (println ta)
-            (+ (abs (+ x11 (* ta (- x12 x11)))) (abs (+ y11 (* ta (- y12 y11))))))
-          (do
-            (println "no")
-            1000000))))
-    ))
+      (let [t (nth ins i)
+            n (Integer. (re-find #"\d+" t))
+            dir (str (first t))]
+        (recur (concat s (repeat n (movement dir))) (inc i))))))
 
 (defn create-and-intersect [i]
   (let [l1 (create-line (first i))
         l2 (create-line (second i))]
-    (loop [closest 1000000
-           idx 0]
-      (if (and (< idx (count l1)) (< idx (count l2)))
-        (let [l1s (nth l1 idx) l2s (nth l2 idx)]
-          (let [dis (intersects l1s l2s)]
-            (if (and (> closest dis) (not (= dis nil)))
-              (recur dis (inc idx))
-              (recur closest (inc idx)))))
-        (println closest)))))
+    (println (apply clojure.set/intersection (set l1) (set l2)))))
 
-(create-and-intersect (read-and-split "day 3/input.txt"))
+(create-and-intersect input)
