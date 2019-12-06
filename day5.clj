9d3dd1f7ce1d93a45cfcefabbbf3866519e84278
Sam Gates <Sam.Gates@heliosinteractive.com>
2019-12-05 21:49:16 -0500

    Day 5 Part 2 complete!

--- day5.clj
+++ day5.clj
@@ -15,35 +15,30 @@
    7 4
    8 4})
 
-(defn jumpif [c i n c] (if c [n c] [i c]))
-
 (defn value [c o n d]
-  ;(do (println (str o " and " d)))
   (if d (case o
-          0  (nth c n) ;(do (println (str (nth c n) "\n" c)))
+          0  (nth c n)
           1 n)
       n))
 
 (defn execute [s c m i]
-  (println s)
   (let [o (opcode (first s))
         d (steps (last o))
         ni (+ i d)
-        n1 (value c (nth o 2) (nth s 1) (= d 4))
-        n2 (if (<= 3 (count s)) (value c (nth o 1) (nth s 2) (= d 4)) nil)
-        n3 (if (<= 4 (count s)) (value c (nth o 0) (nth s 3) (some (partial = (last o)) [5 6 7 8])) nil)]
-    (do (println (str n1 " : " n2 " : " n3))
-        (case (last o)
-          1 [ni (assoc c n3 (+ n1 n2))]
-          2 [ni (assoc c n3 (* n1 n2))]
-          3 [ni (assoc c n1 m)]
-          4 (do
-              (println (str "Output: " (nth c n1)))
-              [ni c])
-          5 (if (not (zero? n1)) [n2 c] [ni c])
-          6 (if (zero? n1) [n2 c] [ni c])
-          7 [ni (assoc c n3 (if (< n1 n2) 1 0))]
-          8 [ni (assoc c n3 (if (= n1 n2) 1 0))]))))
+        n1 (value c (nth o 2) (nth s 1) (or (= d 3) (= d 4)))
+        n2 (if (<= 3 (count s)) (value c (nth o 1) (nth s 2) (or (= d 3) (= d 4))) nil)
+        n3 (if (<= 4 (count s)) (value c (nth o 0) (nth s 3) (some (partial = (last o)) [5 6])) nil)]
+    (case (last o)
+      1 [ni (assoc c n3 (+ n1 n2))]
+      2 [ni (assoc c n3 (* n1 n2))]
+      3 [ni (assoc c n1 m)]
+      4 (do
+          (println (str "Output: " (nth c n1)))
+          [ni c])
+      5 (if (not (zero? n1)) [n2 c] [ni c])
+      6 (if (zero? n1) [n2 c] [ni c])
+      7 [ni (assoc c n3 (if (< n1 n2) 1 0))]
+      8 [ni (assoc c n3 (if (= n1 n2) 1 0))])))
 
 (defn solve [c m]
   (loop [i 0 n c]
@@ -56,4 +51,4 @@
                   o (execute s n m i)]
               (recur (first o) (second o))))))))
 
-(time (solve (u/input-csv 0) 0))
+(time (solve (u/input-csv 5) 5))
