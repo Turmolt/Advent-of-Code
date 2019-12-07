f125fa9e34859d5ddcad1e2cc81c036baa4f0c29
Sam Gates <sam.gates@heliosinteractive.com>
2019-12-07 18:29:39 -0500

    Slightly better but not good

--- day7.clj
+++ day7.clj
@@ -11,40 +11,35 @@
                      (map #(cons x %) (permutations (remove #{x} s)))))
      [s])))
 
-(defn solve-phase [c m p]
+(defn solve-phase [c m p cmd]
   (let [op (cpu/opcode (nth c 0))
         step (cpu/steps (last op))
         s (subvec c 0 step)]
     (->> (cpu/execute s c p 0)
          (second)
-         (#(cpu/solve % m step)))))
-
-(defn solve-phase-interuptable [c m p]
-  (let [op (cpu/opcode (nth c 0))
-        step (cpu/steps (last op))
-        s (subvec c 0 step)]
-        (->> (cpu/execute s c p 0)
-             (second)
-             (#(cpu/solve-interuptable % m step)))))
+         (#(cmd % m step)))))
 
 (defn check-phase [c m p]
     (loop [i m n (first p) r (rest p)]
       (if-not (nil? n)
-        (let [s (solve-phase c i n)]
+        (let [s (solve-phase c i n cpu/solve)]
             (recur s (first r) (rest r)))
         [i p])))
 
 (defn check-phase-recursively [c m p]
-    (loop [i m n (first p) r (rest p) mem (vec (take 5 (repeat [0 c]))) memind 0 coll []]
+    (loop [i m n (first p) r (rest p) mem
+           (vec (take 5 (repeat [0 c]))) mi 0 coll []]
           (if (nil? i)
             coll
           (if-not (nil? n)
-              (let [s (solve-phase-interuptable c i n)]
-                (recur (first s) (first r) (rest r) (assoc mem memind s) (mod (inc memind) 5) (if (= 4 memind) (conj coll (first s)) coll)))
-              (let [s (cpu/solve-interuptable (second (nth mem memind)) i (last (nth mem memind)))]
+              (let [s (solve-phase c i n cpu/solve-interuptable)]
+                (recur (first s) (first r) (rest r) (assoc mem mi s)
+                       (mod (inc mi) 5) (if (= 4 mi) (conj coll (first s)) coll)))
+              (let [s (cpu/solve-interuptable (second (nth mem mi)) i (last (nth mem mi)))]
                   (if (nil? (first s))
                     coll
-                    (recur (first s) nil nil (assoc mem memind s) (mod (inc memind) 5) (if (= 4 memind) (conj coll (first s)) coll))))))))
+                    (recur (first s) nil nil (assoc mem mi s)
+                           (mod (inc mi) 5) (if (= 4 mi) (conj coll (first s)) coll))))))))
 
 (defn part-one [] (apply max (map (comp first #(check-phase (u/input-csv 7) 0 %)) (vec (map vec (permutations [0 1 2 3 4]))))))
 
