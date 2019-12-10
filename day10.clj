d90cb06ae8f5a7a79da66ff798b9abd67650d057
Sam Gates <Sam.Gates@heliosinteractive.com>
2019-12-10 18:09:27 -0500

    refactored day 10!

--- day10.clj
+++ day10.clj
@@ -4,24 +4,22 @@
 
 (def asteroid-field (remove nil? (str/split-lines (u/input 10))))
 
-(def station [31 20])
-
-(defn calc-angle [[x1 y1] [x2 y2]]
-  (->> (Math/atan2 (- y2 y1) (- x2 x1))
-       (* (/ 180 Math/PI))))
-
-(defn coord [width idx itm]
+(defn get-coord [width idx itm]
   [[(mod idx width) (int (/ idx width))] itm])
 
 (defn map-field [field]
   (let [width (count (first field))]
     (->> (flatten field)
          (#(flatten (map vec %)))
-         (map-indexed (partial coord width))
+         (map-indexed (partial get-coord width))
          (remove (fn [x] (= \. (second x))))
          (map first))))
 
 (def asteroids (map-field asteroid-field))
+
+(defn calc-angle [[x1 y1] [x2 y2]]
+  (->> (Math/atan2 (- y2 y1) (- x2 x1))
+       (* (/ 180 Math/PI))))
 
 (defn find-angle [coordinates start]
   (map (partial calc-angle start) (vec (remove (partial = start) coordinates))))
@@ -41,14 +39,22 @@
                   (partial find-angles asteroids)))
        (reduce (fn [x y] (if (> (second x) (second y)) x y)))))
 
-;(time (part-one))
+(defn part-two [station]
+  (->> asteroids
+       (remove (partial = station))
+       (vec)
+       (map (partial find-angles-indexed-distance station))
+       (sort-by #(mod (- (second %) 90) 360))
+       (partition-by second)
+       (map reverse)
+       (#(last (nth % 199)))
+       (last)))
+
+(time (part-one))
 ;; => [[31 20] 319]
 ;; => "Elapsed time: 206.7806 msecs"
 
-(defn part-two []
-  (map (partial find-angles-indexed-distance station) (vec (remove (partial = station) asteroids))))
 
-(defn p2 []
-  (map reverse (partition-by second (sort-by #(mod (- (second %) 90) 360) (part-two)))))
-
-(nth (p2) 199)
+(time (part-two [31 20]))
+;; => [5 17]
+;; => "Elapsed time: 2.8153 msecs"
