4d8364b1704698e5cd0a8e96cdfdca0bae5a4509
Sam Gates <Sam.Gates@heliosinteractive.com>
2019-12-12 15:37:29 -0500

    A non-working solution to part b.. hooray

--- day12.clj
+++ day12.clj
@@ -2,7 +2,7 @@
   (:require [adventofcode.util :as u]
             [clojure.string :as str]))
 
-(def input (->> (u/input 12)
+(def input (->> (u/input 0)
                 (#(str/replace % #"\<" ""))
                 (#(str/replace % #"\>" ""))
                 (str/split-lines)
@@ -31,17 +31,12 @@
 
 (defn parse-into-moons [raw] (map-indexed create-moon raw))
 
-(def resumed-moons '({:position {:x -45.0, :y 34.0, :z -15.0}, :velocity {:x -8.0, :y 2.0, :z -12.0}, :id 0}
-                     {:position {:x 49.0, :y 90.0, :z 48.0}, :velocity {:x 15.0, :y -8.0, :z 0.0}, :id 1}
-                     {:position {:x -1.0, :y -83.0, :z -42.0}, :velocity {:x -4.0, :y 8.0, :z -6.0}, :id 2}
-                     {:position {:x -1.0, :y -3.0, :z -28.0}, :velocity {:x -3.0, :y -2.0, :z 18.0}, :id 3}))
-
 (def parsed-moons (parse-into-moons input))
 
 (defn calculate-velocity-delta [target other-moon]
-  (let [dx (- (position other-moon :x) (position target :x))
-        dy (- (position other-moon :y) (position target :y))
-        dz (- (position other-moon :z) (position target :z))]
+  (let [dx (compare (position other-moon :x) (position target :x))
+        dy (compare (position other-moon :y) (position target :y))
+        dz (compare (position other-moon :z) (position target :z))]
     {:x (u/sign dx) :y (u/sign dy) :z (u/sign dz)}))
 
 ;this is yucky.. but i don't know a better clojure way to do it yet. reduce [[1 2] [1 2] [1 2]] => [3 6]
@@ -66,16 +61,27 @@
 (defn update-moon [moons target]
   (update-position (update-velocity target moons)))
 
-(defn time-step [input-moons num-steps]
-  (loop [moons input-moons iterations 0]
-    (if (>= iterations num-steps) moons
-        (recur (map (partial update-moon moons) moons) (inc iterations)))))
-
-(defn part-one []
-  (time-step resumed-moons 60))
-
 (defn reduce-energy [moon]
   (* (+ (Math/abs (position moon :x)) (Math/abs (position moon :y)) (Math/abs (position moon :z)))
-    (+ (Math/abs (velocity moon :x)) (Math/abs (velocity moon :y)) (Math/abs (velocity moon :z)))))
+     (+ (Math/abs (velocity moon :x)) (Math/abs (velocity moon :y)) (Math/abs (velocity moon :z)))))
 
-(reduce + (map reduce-energy (part-one)))
+(defn part-one []
+  (->> (loop [moons parsed-moons iterations 0]
+         (if (>= iterations 1000) moons
+             ;(do (println moons))
+             (recur (mapv (partial update-moon moons) moons) (inc iterations))))
+       (map reduce-energy)
+       (reduce +)))
+
+(defn part-two [axis]
+  (let [original (map #(list (position % axis) (velocity % axis)) parsed-moons)]
+    (loop [moons parsed-moons iterations 0]
+      (let [present (map #(list (position % axis) (velocity % axis)) moons)]
+        (if (and (= original present) (> iterations 0)) iterations
+            (recur (mapv (partial update-moon moons) moons) (inc iterations)))))))
+
+;(map #(list (position % :z) (velocity % :z)) parsed-moons)
+
+;(part-one)
+
+;(part-two :x)
